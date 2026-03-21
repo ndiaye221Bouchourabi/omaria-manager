cefc50f4293928eb733f559fef8ea16773afad98
Cheikh Ndiaye <ndiaye.cheikhh20@gmail.com>
2026-03-21 10:14:12 +0000

    fix: clear route cache on startup

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 sleep 5
 php artisan migrate --force
 php artisan db:seed --class=AdminSeeder --force
+php artisan route:clear
 php artisan queue:work --tries=3 &
 php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
